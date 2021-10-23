d05c1c8193994cc9013ad5327cfef6bb52b58551
joaoMarinho94 <joaopaulim94@gmail.com>
2021-10-23 15:03:02 -0300

    refactor: refactor component Modal class to fc and add typescript

---
+++ src/components/Modal/index.tsx
@@ -0,0 +1,37 @@
+import ReactModal from "react-modal";
+
+interface Props {
+  isOpen: boolean;
+  setIsOpen: () => void;
+}
+
+export const Modal: React.FC<Props> = ({ isOpen, setIsOpen, children }) => {
+  return (
+    <ReactModal
+      shouldCloseOnOverlayClick={!false}
+      onRequestClose={setIsOpen}
+      isOpen={isOpen}
+      ariaHideApp={false}
+      style={{
+        content: {
+          top: "50%",
+          left: "50%",
+          right: "auto",
+          bottom: "auto",
+          marginRight: "-50%",
+          transform: "translate(-50%, -50%)",
+          background: "#F0F0F5",
+          color: "#000000",
+          borderRadius: "8px",
+          width: "736px",
+          border: "none",
+        },
+        overlay: {
+          backgroundColor: "#121214e6",
+        },
+      }}
+    >
+      {children}
+    </ReactModal>
+  );
+};
