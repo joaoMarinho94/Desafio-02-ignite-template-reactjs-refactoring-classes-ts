1650e0cb71f0afe376e8accf816c17b7b8a9a4b7
joaoMarinho94 <joaopaulim94@gmail.com>
2021-10-23 11:50:01 -0300

    feat: add typescript

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+// import { BrowserRouter as Router } from 'react-router-dom';
+
+// import Routes from './routes';
+
+// import GlobalStyle from './styles/global';
+
+export const App: React.FC = () => {
+  return (
+    <>
+      {/* <GlobalStyle />
+      <Router>
+        <Routes />
+      </Router> */}
+      <h1>teste</h1>
+    </>
+  );
+};
